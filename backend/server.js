7e1f3d6ef73308debe73c698d55b8c2c9ef54749
kabra-palak <kabrapalak08@gmail.com>
2026-03-20 11:18:46 +0530

    Initial backend setup - server running

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express, { json } from "express";
+import passport from "passport";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { config } from "dotenv";
+config();
+import session from "express-session";
+import http from "http";
+
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  })
+);
+
+app.use(json());
+app.use(cookieParser());
+app.use(passport.initialize());
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+  server.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
